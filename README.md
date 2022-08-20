654df0347954573406f7266bd4a7a36cf98fe3f5
Daniil <dan.dad@bk.ru>
2022-08-20 19:04:14 +0300

    docker-compose up works

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # drf_simple_blog
 DRF + Docker + PostgreSQL 
+
+
+docker-compose exec web python ./simple_blog/manage.py migrate --noinput
--- simple_blog/api/admin.py
+++ simple_blog/api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import TestModel, TestModelsTwo
 # Register your models here.
+
+
+@admin.register(TestModel)
+class TestModel(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'second_name'
+    )
+    
+@admin.register(TestModelsTwo)
+class TestModelsTwo(admin.ModelAdmin):
+    list_display = (
+        'organisation',
+        'adress'
+    )
